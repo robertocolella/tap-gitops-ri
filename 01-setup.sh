064e601a884cc58385e053125ca085df19c075ac
Gorkem Ozlu <45913400+gorkemozlu@users.noreply.github.com>
2023-04-19 14:37:35 +0300

    update for TAP 1.5 GA release

--- 01-setup.sh
+++ 01-setup.sh
@@ -27,20 +27,20 @@ rm -rf clusters
 rm -rf setup-repo.sh
 
 # GitOps Ref. Implementation
-pivnet download-product-files --product-slug='tanzu-application-platform' --release-version='1.5.0-rc.14' --product-file-id=1459284
-tar -xvf tanzu-gitops-ri-0.0.3.tgz
+pivnet download-product-files --product-slug='tanzu-application-platform' --release-version='1.5.0' --product-file-id=1467377
+tar -xvf tanzu-gitops-ri-*.tgz
 ./setup-repo.sh full-profile sops
 
 # Download Cluster-Essentials
 
-uname -s| grep Darwin && pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.4.1' --product-file-id=1423996 && mv tanzu-cluster-essentials-darwin-amd64-1.4.1.tgz gorkem/
-uname -s| grep Linux && pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.4.1' --product-file-id=1423994 && mv tanzu-cluster-essentials-darwin-amd64-1.4.1.tgz gorkem/
+uname -s| grep Darwin && pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.5.0' --product-file-id=1460874 && mv tanzu-cluster-essentials-darwin-amd64-1.5.0.tgz gorkem/
+uname -s| grep Linux && pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.5.0' --product-file-id=1460876 && mv tanzu-cluster-essentials-darwin-amd64-1.5.0.tgz gorkem/
 
 mkdir gorkem/tanzu-cluster-essentials
-tar -xvf gorkem/tanzu-cluster-essentials-darwin-amd64-1.4.1.tgz -C gorkem/tanzu-cluster-essentials
-cp ./gorkem/templates/values-template.yaml ./gorkem/templates/values.yaml
+tar -xvf gorkem/tanzu-cluster-essentials-darwin-amd64-1.5.0.tgz -C gorkem/tanzu-cluster-essentials
+cp ./gorkem/templates/values-template.yaml ./gorkem/values.yaml
 
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:2354688e46d4bb4060f74fca069513c9b42ffa17a0a6d5b0dbb81ed52242ea44
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:79abddbc3b49b44fc368fede0dab93c266ff7c1fe305e2d555ed52d00361b446
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=$(yq '.tanzuNet_username' gorkem/values.yaml)
 export INSTALL_REGISTRY_PASSWORD=$(yq '.tanzuNet_password' gorkem/values.yaml)
@@ -82,7 +82,7 @@ sops --encrypt ./gorkem/tmp-enc/tap-sensitive-values.yaml > ./gorkem/tmp-enc/tap
 mv ./gorkem/tmp-enc/tap-sensitive-values.sops.yaml ./clusters/full-profile/cluster-config/values
 
 
-ytt --ignore-unknown-comments -f ./gorkem/templates/values.yaml -f ./gorkem/templates/tap-non-sensitive-values-template.yaml > ./clusters/full-profile/cluster-config/values/tap-values.yaml
+ytt --ignore-unknown-comments -f ./gorkem/values.yaml -f ./gorkem/templates/tap-non-sensitive-values-template.yaml > ./clusters/full-profile/cluster-config/values/tap-values.yaml
 
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=$(yq '.tanzuNet_username' ./gorkem/values.yaml)
