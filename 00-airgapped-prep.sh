3d651048635be980e3cf7053a3f69bf93dfba0ec
Gorkem Ozlu <spammailatmayin@gmail.com>
2023-08-02 07:59:46 +0300

    update gen-cert

--- 00-airgapped-prep.sh
+++ 00-airgapped-prep.sh
@@ -281,23 +281,36 @@ elif [ "$1" = "gen-cert" ]; then
     mkdir -p cert/
     cd cert
     export DOMAIN=*.$INGRESS_DOMAIN
-    
     export SUBJ="/C=TR/ST=Istanbul/L=Istanbul/O=Customer, Inc./OU=IT/CN=${DOMAIN}"
     openssl genrsa -des3 -out ca.key -passout pass:1234 4096
-    openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -passin pass:1234 -addext "keyUsage=critical, digitalSignature, cRLSign, keyCertSign" -addext "basicConstraints=critical,CA:true" -out ca.crt -subj "$SUBJ"
+cat > ca.conf <<-EOF
+[req]
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+C = TR
+ST = Istanbul
+L = Istanbul
+O = Customer, Inc.
+OU = IT
+CN = $DOMAIN
+[ca]
+basicConstraints=CA:TRUE
+keyUsage=critical, digitalSignature, keyCertSign, cRLSign
+EOF
+    openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -passin pass:1234 -subj "$SUBJ" -extensions ca -config ca.conf -out ca.crt
     openssl genrsa -out server-app.key 4096
     openssl req -sha512 -new \
           -subj "$SUBJ" \
           -key server-app.key \
           -out server-app.csr
 cat > v3.ext <<-EOF
-  authorityKeyIdentifier=keyid,issuer
-  basicConstraints=CA:FALSE
-  keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-  extendedKeyUsage = serverAuth
-  subjectAltName = @alt_names
-  [alt_names]
-  DNS.1=${DOMAIN}
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1=${DOMAIN}
 EOF
     openssl x509 -req -sha512 -days 3650 \
           -passin pass:1234 \
@@ -306,6 +319,20 @@ EOF
           -in server-app.csr \
           -out server-app.crt
     openssl rsa -in ca.key -out ca-no-pass.key -passin pass:1234
+    md5crt=$(openssl x509 -modulus -noout -in server-app.crt | openssl md5|awk '{print $2}')
+    md5key=$(openssl rsa -noout -modulus -in server-app.key | openssl md5|awk '{print $2}')
+    echo $md5crt
+    echo $md5key
+    if [ "$md5crt" == "$md5key" ] ;
+        then
+            echo "Certificates generated successfully"
+            #exit 0
+        else
+            echo "Certificate md5's mismatch. Error."
+            exit 1
+    fi
+
+
     cd ..
 else
     echo "Invalid parameter: $1"
